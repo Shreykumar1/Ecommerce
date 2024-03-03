0c3aed19c7cb8ebb8959d20be54d040468eba770
Shrey Kumar <shreykumar191@gmail.com>
2024-03-03 17:45:56 +0530

    Add Product Page in Frontend

--- client/src/components/cart/CartTotals.jsx
+++ client/src/components/cart/CartTotals.jsx
@@ -40,7 +40,7 @@ const CartTotals = ({type,handleType}) => {
             <div className='bg-base-200 mt-4 card p-4'>
                 <label htmlFor="paymentType" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Payment Type</label>
                 <select id="paymentType" value={type}  onChange={handleType} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                    <option  value="default">Choose a Payment Type</option>
+                    <option selected value="default">Choose a Payment Type</option>
                     <option value="upi">Upi</option>
                     <option value="cash">Cash</option>
                     <option value="card">Card</option>
--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -4,15 +4,18 @@ const path = require('path');
 
 const uploadProductImage = async (req,res) => {
     if(!req.files){
-        throw new Error("Upload File");
+       return res.status(400).send({image : {msg : 'Please Upload Image',src : ""}})
+        // throw new Error("Upload File");
     }
     let productImage = req.files.image;
     if(!productImage.mimetype.startsWith('image')){
-        throw new Error('Please Upload Image');
+        // throw new Error('Please Upload Image');
+      return  res.status(400).send({image : {msg : 'Please Upload Image',src : ""}})
     }
     const maxSize = 1024 * 1024
     if(productImage.size >  maxSize){
-        throw new Error('Please Upload Image Smaller than 1MB');
+      return  res.status(400).send({image : {msg : 'Please Upload Image Smaller Than 1MB',src : ""}})
+        // throw new Error('Please Upload Image Smaller than 1MB');
     }
 
     const imagePath = path.join(__dirname,'../public/uploads/'+`${productImage.name}`);
