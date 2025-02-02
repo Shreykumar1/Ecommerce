984180418e3ab4602415c30fe2cbc107126bc4df
Shrey Kumar <shreykumar854@gmail.com>
2025-02-02 16:20:16 +0530

    Connection

--- db/connect.js
+++ db/connect.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host : process.env.HOST,
-    user : process.env.USER,
-    password : process.env.PASSWORD,
-    database : process.env.DATABASE,
-    port : process.env.PORT
+    host : "junction.proxy.rlwy.net",
+    user : "root",
+    password : "BgXIoEPDfEIjjSHgTEuHIkIPDJbqDOho",
+    database : "railway",
+    port : "38945"
 });
 
 module.exports = pool.promise();
